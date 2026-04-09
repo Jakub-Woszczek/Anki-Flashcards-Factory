61fb2818cfb11e063de04f0baa74977f6164a5d6
Jakub Woszczek <icefreestyle2000@gmail.com>
2026-04-09 13:25:02 +0200

    Updated handling of gemini asistant if no api key was provided

--- external_APIs/geminiAPI.py
+++ external_APIs/geminiAPI.py
@@ -6,22 +6,37 @@ from rich.markdown import Markdown
 
 class GeminiBot:
     def __init__(self):
-        self.client = genai.Client()  # Lib will pick up api key automatically
+        self.api_key = os.getenv("GEMINI_API_KEY")
+        if self.api_key:
+            self.client = genai.Client()
+        else:
+            self.client = None  # No client if key missing
+
         self.model = "gemini-3-flash-preview"
         self.bot_question = "Napisz jedno zdanie o jednorożcu"
         self.console = Console()
 
     def get_answer(self, question):
-        response = self.client.models.generate_content(
-            model=self.model,
-            contents=question,
-        )
-        return response.text
+        if not self.client:
+            return "no api key provided"
+
+        try:
+            response = self.client.models.generate_content(
+                model=self.model,
+                contents=question,
+            )
+            return response.text
+        except Exception as e:
+            return f"error: {str(e)}"
 
     def get_word_translation(self, word):
         print(f"Tłumaczenie słowa {word} ...")
-        prompt = f"Przetłumacz słowo {word} na język polski"
-        description = self.get_answer(prompt)
+
+        description = self.get_answer(f"Przetłumacz słowo {word} na język polski")
+        if description == "no api key provided":
+            print(description)
+            return description
+
         markdown_renderable = Markdown(description)
         self.console.print(markdown_renderable)
         return description
